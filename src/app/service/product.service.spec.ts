0a1fd0400db7c4f8ba57ed73117e31100a6b0c72
Raoul DIFFOUO <diraulo@gmail.com>
2017-11-09 17:32:38 +0200

    create specs for ProductService

--- src/app/service/product.service.spec.ts
+++ src/app/service/product.service.spec.ts
@@ -1,15 +1,76 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { HttpModule, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+
+import { JsonApiModule } from 'angular2-jsonapi';
+
+import { DatastoreService } from './datastore.service';
 import { ProductService } from './product.service';
 
-describe('ProductService', () => {
+describe('MockBackend: ProductService', () => {
+  let mockBackend, service, products;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ProductService]
+      imports: [ HttpModule ],
+      providers: [
+        DatastoreService,
+        MockBackend,
+        ProductService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
     });
   });
 
-  it('should be created', inject([ProductService], (service: ProductService) => {
-    expect(service).toBeTruthy();
+  beforeEach(inject([MockBackend, ProductService], (_mockBackend, _productService) => {
+    mockBackend = _mockBackend;
+    service = _productService;
   }));
+
+  beforeEach(() => {
+    products = [{
+      "id": "1",
+      "type": "products",
+      "attributes": {
+        "name": "coke",
+        "price": 10,
+        "description": "refreshing",
+        "image": "image",
+        "status": true,
+        "type": "drink"
+      }
+    }];
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('getAll: should return a list of products', () => {
+    mockBackend.connections.subscribe(connection => {
+      connection.mockRespond(new Response({
+        body: JSON.stringify(products)
+      }))
+    })
+
+    service.getAll().subscribe(_products => {
+      expect(_products[0]).toEqual(products);
+      expect(_products.length).toBe(1);
+    })
+  })
+
+  it('show: returns a single product given its id', () => {
+    let response = products[0];
+
+    mockBackend.connections.subscribe(connection => {
+      connection.mockRespond(new Response({
+        body: JSON.stringify(response)
+      }))
+    })
+
+    service.show(1).subscribe(product => {
+      expect(product).toEqual(response);
+    })
+  })
 });
