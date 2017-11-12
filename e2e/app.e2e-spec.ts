10a694cbf90670449e85ba89a6a96746112569a9
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-12 16:30:45 +0100

    removed unused test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,15 +7,10 @@ describe('kimchistan-front-end App', () => {
     page = new AppPage();
   });
 
-  xit('should display welcome message', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
-
-  // it('should find dropdown for Bibimbap', () => {
-  //   page.navigateTo();
-  //   element(by.model())
-  // });
 });
 
 
