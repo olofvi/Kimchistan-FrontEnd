2c2d6fa9861ff3babe89378d65d73ca6c7c96daa
olofvi <olofvi89@gmail.com>
2017-11-14 22:05:12 +0100

    payment service test passing

--- src/app/service/payment.service.spec.ts
+++ src/app/service/payment.service.spec.ts
@@ -1,11 +1,38 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { PaymentService } from './payment.service';
+import { MockBackend } from '@angular/http/testing';
+import { DatastoreService } from './datastore.service';
+import { HttpModule, XHRBackend } from '@angular/http';
 
-describe('PaymentService', () => {
+
+
+
+describe('MockBackend: PaymentService', () => {
+  let mockBackend, service, payments;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PaymentService]
+      imports: [ HttpModule ],
+      providers: [
+        DatastoreService,
+        MockBackend,
+        PaymentService,
+        { provide: XHRBackend, useClass: MockBackend }
+      ]
     });
+  });
+
+  beforeEach(inject([MockBackend, PaymentService], (_mockBackend, _paymentService) => {
+    mockBackend = _mockBackend;
+    service = _paymentService;
+  }));
+
+  beforeEach(() => {
+    payments = [{
+      "type": "payments",
+      "email" : "olofvi89@gmail.com",
+      "token" : "tok_1BO9zFCiJmOu5UHgpZLpxCM4"}
+    ]
   });
 
   it('should be created', inject([PaymentService], (service: PaymentService) => {
