e4ae75ee2c74e96e2eb1a703662bd016b972678c
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-16 09:47:07 +0100

    removed localhost server

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,11 @@ describe('kimchistan-front-end App', () => {
     page = new AppPage();
   });
 
+  afterEach(function () {
+    browser.get('/');
+    browser.executeScript('localStorage.clear();');
+  });
+
   it('should find dropdown for Bibimbap, salad and drinks', () => {
     page.navigateTo();
     browser.wait(function () {
@@ -34,10 +39,10 @@ describe('kimchistan-front-end App', () => {
     element(by.id('Bibimbap_Tofu')).click();
 
     expect(accordion.isPresent()).toBeTruthy();
-    accordion.getText().then(function(text){expect(text).toContain('1 in cart (116 kr)')});
+    accordion.getText().then(function(text){expect(text).toContain('2 i korgen (248 kr)')});
 
-    expect(element(by.linkText('Show/hide order')).getTagName()).toBe('a');
-    element(by.linkText('Show/hide order')).click();
+    expect(element(by.linkText('Visa/göm beställningsdetaljer')).getTagName()).toBe('a');
+    element(by.linkText('Visa/göm beställningsdetaljer')).click();
     let order_showing = element(by.id('show_hide'));
     expect(order_showing.isPresent()).toBeFalsy();
   });
