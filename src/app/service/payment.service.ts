1239fa9f41408e4728ed6a460bfc684b5ff95b0c
Magnús Þór Ársælsson <magnus.thor.ar@gmail.com>
2017-11-18 19:13:24 +0100

    Increase test coverage (#32)
    * added tests for loading cart
    
    * shoppingcart service is fully tested
    
    * removed unused method
    
    * added payment create method back in
    
    * removed payment create method

--- src/app/service/payment.service.ts
+++ src/app/service/payment.service.ts
@@ -10,18 +10,8 @@ export class PaymentService {
   constructor(private datastore: DatastoreService, private http: Http) {
   }
 
-  create(token: any, amount: any) {
-    const payment = this.datastore.createRecord(Payment, {
-      email: token.email,
-      token: token.id,
-      amount: amount,
-    });
-
-    return payment.save();
-  }
-
   createPayment(token: any, amount: any) {
-    const apiUrl = 'https://kimchistan-api.herokuapp.com/api/v1'
+    const apiUrl = 'https://kimchistan-api.herokuapp.com/api/v1';
     const payload = {
       data: {
         attributes: {
@@ -30,7 +20,7 @@ export class PaymentService {
           amount: amount
         }
       }
-    }
+    };
 
     return this.http.post(`${apiUrl}/payments`, payload)
       .map(res => res.json());
